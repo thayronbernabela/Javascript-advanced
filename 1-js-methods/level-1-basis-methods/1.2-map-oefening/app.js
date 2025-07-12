74f932269c7cd1932e69f4f4d6dc28f39d5b8853
9024514 <9024514@student.zadkine.nl>
2025-07-12 13:42:38 +0200

    opdr1,2

--- 1-js-methods/level-1-basis-methods/1.2-map-oefening/app.js
+++ 1-js-methods/level-1-basis-methods/1.2-map-oefening/app.js
@@ -5,7 +5,9 @@ const namen = ['sanne', 'johan', 'sjoerd'];
 console.log('Originele namen:', namen);
 
 // TODO: Zet alle namen om naar hoofdletter
-const hoofdletterNamen = /* jouw code hier */
+const hoofdletterNamen = namen.map(naam => 
+  naam.charAt(0).toUpperCase() + naam.slice(1)
+);
 
 console.log('Namen met hoofdletter:', hoofdletterNamen);
 console.log('Verwacht resultaat: [\'Sanne\', \'Johan\', \'Sjoerd\']');
