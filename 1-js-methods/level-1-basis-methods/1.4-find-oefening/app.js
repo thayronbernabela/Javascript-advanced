a89922c9a796fb44c98e8bdf6b97850006af4527
9024514 <9024514@student.zadkine.nl>
2025-07-12 13:45:16 +0200

    opdr1.4

--- 1-js-methods/level-1-basis-methods/1.4-find-oefening/app.js
+++ 1-js-methods/level-1-basis-methods/1.4-find-oefening/app.js
@@ -5,11 +5,11 @@ const getallen = [12, 45, 67, 23, 89, 34];
 console.log('Getallen array:', getallen);
 
 // TODO: Zoek het eerste getal groter dan 50
-const gevondenGetal = /* jouw code hier */
+const gevondenGetal = getallen.find(getal => getal > 50);
 
 console.log('Eerste getal > 50:', gevondenGetal);
 
 // TODO: Zoek de index van het eerste getal groter dan 50
-const gevondenIndex = /* jouw code hier */
+const gevondenIndex = getallen.findIndex(getal => getal > 50);
 
 console.log('Index van eerste getal > 50:', gevondenIndex);
