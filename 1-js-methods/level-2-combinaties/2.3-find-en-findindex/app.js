969c5c66c8b3ce7e723a8401d0f46a77d335b5c9
9024514 <9024514@student.zadkine.nl>
2025-07-13 15:10:56 +0200

    opdr2.3

--- 1-js-methods/level-2-combinaties/2.3-find-en-findindex/app.js
+++ 1-js-methods/level-2-combinaties/2.3-find-en-findindex/app.js
@@ -8,17 +8,17 @@ console.log('Temperaturen:', temperaturen);
 console.log('Kleuren:', kleuren);
 
 // TODO: Zoek de eerste temperatuur boven 25 graden
-const warmeTemperatuur = /* jouw code hier */
+const warmeTemperatuur = temperaturen.find(temp => temp > 25);
 console.log('Eerste warme temperatuur:', warmeTemperatuur);
 
 // TODO: Zoek de index van de kleur 'groen'
-const groenIndex = /* jouw code hier */
+const groenIndex = kleuren.findIndex(kleur => kleur === 'groen');
 console.log('Index van groen:', groenIndex);
 
 // TODO: Zoek de eerste temperatuur onder 20 graden
-const koudeTemperatuur = /* jouw code hier */
+const koudeTemperatuur = temperaturen.find(temp => temp < 20);
 console.log('Eerste koude temperatuur:', koudeTemperatuur);
 
 // TODO: Zoek de index van de eerste temperatuur boven 23 graden
-const warmeIndex = /* jouw code hier */
+const warmeIndex = temperaturen.findIndex(temp => temp > 23);
 console.log('Index van eerste warme temperatuur:', warmeIndex);
