ff6a562cc559a352e22cfef573691e04d4cf9561
9024514 <9024514@student.zadkine.nl>
2025-07-13 15:21:59 +0200

    2.4 en 3.1

--- 1-js-methods/level-2-combinaties/2.4-complexe-filtering/app.js
+++ 1-js-methods/level-2-combinaties/2.4-complexe-filtering/app.js
@@ -8,14 +8,13 @@ console.log('Alle getallen:', getallen);
 console.log('Alle woorden:', woorden);
 
 // TODO: Filter getallen die groter zijn dan 10 EN kleiner dan 20
-const middelsteGetallen = /* jouw code hier */
+const middelsteGetallen = getallen.filter(getal => getal > 10 && getal < 20);
 console.log('Getallen tussen 10 en 20:', middelsteGetallen);
 
 // TODO: Filter woorden die beginnen met 'a' EN langer zijn dan 5 letters
-const langeAwoorden = /* jouw code hier */
+const langeAwoorden = woorden.filter(woord => woord.startsWith('a') && woord.length > 5);
 console.log('Lange woorden met a:', langeAwoorden);
 
 // TODO: Filter even getallen die groter zijn dan 15
-const groteEvenGetallen = /* jouw code hier */
+const groteEvenGetallen = getallen.filter(getal => getal > 15 && getal % 2 === 0);
 console.log('Grote even getallen:', groteEvenGetallen);
-
--- 1-js-methods/level-3-string-methods/3.1-zoekfunctie/app.js
+++ 1-js-methods/level-3-string-methods/3.1-zoekfunctie/app.js
@@ -6,7 +6,7 @@ console.log('Alle films:', films);
 
 function zoekFilms(films, zoekterm) {
     // TODO: Return films die de zoekterm bevatten
-    return /* jouw code hier */;
+    return films.filter(film => film.toLowerCase().includes(zoekterm.toLowerCase()));
 }
 
 // Test de functie
