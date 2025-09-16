46461ca5f42310e1cb2d7cef80424db86e641772
9024514 <9024514@student.zadkine.nl>
2025-09-16 14:23:47 +0200

    2-01

--- 2-objecten/opdracht-01-arrow-loop/app.js
+++ 2-objecten/opdracht-01-arrow-loop/app.js
@@ -1,3 +1,14 @@
+const marvels = ['Iron Man', 'The Avengers', 'Black Panther', 'Spider-Man: No Way Home'];
+
+const addMarvel = () => {
+    marvels.push("Black Widow");
+};
+
+addMarvel();
+
+for (let film of marvels) {
+    console.log(film)
+}
 // Je gaat even oefenen met het gebruiken van een arrow function en de for/of loop. 
 
 // Maak een array genaamd 'marvels' en plaats hier 4 Marvel films in.
